22ee9160fe43a01f625d86c0bcaddbb4a694bc6d
JasrajSingh01 <jasrajs24@gmail.com>
2022-12-28 21:37:45 +0530

    first commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.instagram.com/saggujasraj/" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i class="uil uil-instagram"></i>
+            </a>
+            <a href="https://twitter.com/JasrajS77477948" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i class="uil uil-twitter-alt"></i>
+            </a>
+            <a href="https://github.com/JasrajSingh01" className="home__social-icon" target="_blank" rel="noreferrer">
+                <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
